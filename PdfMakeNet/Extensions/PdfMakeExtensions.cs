f0b90c0b84443620552c764cb29f6594cf510e8e
Anthony Rivera <anthony_elgueta@hotmail.com>
2019-10-09 00:43:21 -0400

    Mini refactor of pdfmake. Removed pdfmakebase class to pdfmake class and pdfmake methods to the pdfmakeextensions class.

---
+++ PdfMakeNet/Extensions/PdfMakeExtensions.cs
@@ -0,0 +1,197 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace PdfMakeNet
+{
+    public static class PdfMakeExtensions
+    {
+        /// <summary>
+        /// Extension method to help you build tables more faster an easier without the hassle of loops and structure
+        /// </summary>
+        /// <param name="Columns"></param>
+        /// <param name="Rows"></param>
+        /// <returns></returns>
+        public static List<object> AddTableBodyStructure(this PdfMake pdfMake, List<string> Columns, List<object> Rows)
+        {
+            var body = new List<object>
+            {
+                Columns
+            };
+            foreach (var items in Rows)
+            {
+                var values = new List<object>();
+                var type = items.GetType();
+                if (type.GetGenericArguments()[0].IsSimpleType() && !type.IsAnonymousType())
+                {
+                    foreach (var item in (dynamic)items)
+                    {
+                        values.Add(item);
+                    }
+                }
+                else
+                {
+                    foreach (PropertyInfo prop in items.GetType().GetProperties())
+                    {
+                        values.Add(items.GetType().GetProperty(prop.Name).GetValue(items));
+                    }
+                }
+                body.Add(values);
+            }
+            return body;
+        }
+        
+        #region Client Side Content Result
+        /// <summary>
+        /// Sends a json result with the pdf document definition
+        /// </summary>
+        /// <returns></returns>
+        public static ContentResult DocumentDefinitionInBrowser(this PdfMake pdfMake)
+        {
+            return new ContentResult()
+            {
+                Content = pdfMake.GetDocumentDefinition(),
+                ContentType = "application/json",
+                StatusCode = 200
+            };
+        }
+        /// <summary>
+        /// Sends a javascript script to download the pdf document in browser 
+        /// </summary>
+        /// <param name="Filename"></param>
+        /// <returns></returns>
+        public static ContentResult DownloadInBrowser(this PdfMake pdfMake, string Filename)
+        {
+            return new ContentResult()
+            {
+                Content = pdfMake.GetDownloadInBrowser(Filename),
+                ContentType = "application/javascript",
+                StatusCode = 200
+            };
+        }
+
+        /// <summary>
+        /// Sends a javascript script with the specified Iframe selector to display the pdf document
+        /// </summary>
+        /// <param name="IFrameQuerySelector"></param>
+        /// <returns></returns>
+        public static ContentResult EmbedInBrowserIframe(this PdfMake pdfMake, string IFrameQuerySelector)
+        {
+            return new ContentResult()
+            {
+                Content = pdfMake.GetEmbedInBrowserIframe(IFrameQuerySelector),
+                ContentType = "application/javascript",
+                StatusCode = 200
+            };
+        }
+
+        /// <summary>
+        /// Sends a javascript script to open the pdf document in the browser
+        /// </summary>
+        /// <param name="SameWindow"></param>
+        /// <returns></returns>
+        public static ContentResult OpenInBrowser(this PdfMake pdfMake, bool SameWindow)
+        {
+            return new ContentResult()
+            {
+                Content = pdfMake.GetOpenInBrowser(SameWindow),
+                ContentType = "application/javascript",
+                StatusCode = 200
+            };
+        }
+
+        /// <summary>
+        /// Sends a javascript script to prompt the printer with the pdf document
+        /// </summary>
+        /// <param name="SameWindow"></param>
+        /// <returns></returns>
+        public static ContentResult PrintInBrowser(this PdfMake pdfMake, bool SameWindow)
+        {
+            return new ContentResult()
+            {
+                Content = pdfMake.GetPrintInBrowser(SameWindow),
+                ContentType = "application/javascript",
+                StatusCode = 200
+            };
+        }
+        #endregion
+
+        #region String Document Definitions
+        /// <summary>
+        /// Gets the document definition in json
+        /// </summary>
+        /// <returns></returns>
+        public static string GetDocumentDefinition(this PdfMake pdfMake)
+        {
+            return JsonConvert.SerializeObject(pdfMake);
+        }
+
+        /// <summary>
+        /// Gets the javascript script to dowload the pdf document
+        /// </summary>
+        /// <param name="Filename"></param>
+        /// <returns></returns>
+        public static string GetDownloadInBrowser(this PdfMake pdfMake, string Filename)
+        {
+            if (string.IsNullOrWhiteSpace(Filename))
+                throw new ArgumentNullException("The Filename is null, empty or whitespace.");
+
+            if (!Filename.EndsWith(".pdf"))
+                throw new FormatException("The Filename does not end with .pdf extension.");
+
+            return "pdfMake.createPdf(" + pdfMake.GetDocumentDefinition() + ").download(" + Filename + ");";
+        }
+
+        /// <summary>
+        /// Gets the javascript script to display the pdf document in iframe
+        /// </summary>
+        /// <param name="IFrameQuerySelector"></param>
+        /// <returns></returns>
+        public static string GetEmbedInBrowserIframe(this PdfMake pdfMake, string IFrameQuerySelector)
+        {
+            if (string.IsNullOrWhiteSpace(IFrameQuerySelector))
+                throw new ArgumentNullException("The IframeQuerySelector is null, empty or whitespace.");
+
+            return @"pdfMake.createPdf(" + pdfMake.GetDocumentDefinition() + @").getDataUrl(function(dataUrl) {
+                        document.querySelector('" + IFrameQuerySelector + @"').src = dataUrl
+                    });";
+        }
+
+        /// <summary>
+        /// Gets the javascript script to open in window
+        /// </summary>
+        /// <param name="SameWindow"></param>
+        /// <returns></returns>
+        public static string GetOpenInBrowser(this PdfMake pdfMake, bool SameWindow)
+        {
+            if (SameWindow)
+            {
+                return "pdfMake.createPdf(" + pdfMake.GetDocumentDefinition() + ").open({}, window);";
+            }
+            else
+            {
+                return "pdfMake.createPdf(" + pdfMake.GetDocumentDefinition() + ").open({}, window.open('', '_blank'));";
+            }
+        }
+
+        /// <summary>
+        /// Gets the javascript script to prompt the printer with the pdf document
+        /// </summary>
+        /// <param name="SameWindow"></param>
+        /// <returns></returns>
+        public static string GetPrintInBrowser(this PdfMake pdfMake, bool SameWindow)
+        {
+            if (SameWindow)
+            {
+                return "pdfMake.createPdf(" + pdfMake.GetDocumentDefinition() + ").print({}, window);";
+            }
+            else
+            {
+                return "pdfMake.createPdf(" + pdfMake.GetDocumentDefinition() + ").print({}, window.open('', '_blank'));";
+            }
+        }
+        #endregion
+    }
+}
--- PdfMakeNet/Implementations/Pdfmake.cs
+++ PdfMakeNet/Implementations/Pdfmake.cs
@@ -1,159 +1,351 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace PdfMakeNet
 {
-    public class PdfMake : PdfMakeBase, IPdfmake
+    public abstract class PdfMake : IPdfmake<object>
     {
-        #region Client Side Content Result
+        #region Fields
         /// <summary>
-        /// Sends a json result with the pdf document definition
+        /// The header section
         /// </summary>
-        /// <returns></returns>
-        public ContentResult DocumentDefinitionInBrowser()
-        {
-            return new ContentResult()
-            {
-                Content = GetDocumentDefinition(),
-                ContentType = "application/json",
-                StatusCode = 200
-            };
-        }
+        [JsonProperty("header")]
+        public List<object> Header { get; set; }
         /// <summary>
-        /// Sends a javascript script to download the pdf document in browser 
+        /// The footer section
         /// </summary>
-        /// <param name="Filename"></param>
-        /// <returns></returns>
-        public ContentResult DownloadInBrowser(string Filename)
-        {
-            return new ContentResult()
-            {
-                Content = GetDownloadInBrowser(Filename),
-                ContentType = "application/javascript",
-                StatusCode = 200
-            };
-        }
+        [JsonProperty("footer")]
+        public List<object> Footer { get; set; }
+        /// <summary>
+        /// The body section
+        /// </summary>
+        [JsonProperty("content")]
+        public List<object> Body { get; set; }
+        /// <summary>
+        /// The background color section
+        /// </summary>
+        [JsonProperty("background")]
+        public object Background { get; set; }
+        /// <summary>
+        /// The images section
+        /// </summary>
+        [JsonProperty("images")]
+        public object Images { get; set; }
+        /// <summary>
+        /// The default style section
+        /// </summary>
+        [JsonProperty("defaultStyle")]
+        public PdfMakeStyle DefaultStyle { get; set; }
+        /// <summary>
+        /// The style section
+        /// </summary>
+        [JsonProperty("styles")]
+        public object Styles { get; set; }
+        /// <summary>
+        /// The page size option
+        /// </summary>
+        [JsonProperty("pageSize")]
+        public string PageSize { get; set; }
+        /// <summary>
+        /// The page margins option
+        /// </summary>
+        [JsonProperty("pageMargins")]
+        public List<int> PageMargins { get; set; }
+        /// <summary>
+        /// The page orientation option
+        /// </summary>
+        [JsonProperty("pageOrientation")]
+        public string PageOrientation { get; set; }
+        /// <summary>
+        /// The compression option
+        /// </summary>
+        [JsonProperty("compress")]
+        public bool Compress { get; set; }
+        /// <summary>
+        /// The user password option
+        /// </summary>
+        [JsonProperty("userPassword")]
+        public string UserPassword { get; set; }
+        /// <summary>
+        /// The owner password option
+        /// </summary>
+        [JsonProperty("ownerPassword")]
+        public string OwnerPassword { get; set; }
+        /// <summary>
+        /// The permissions section
+        /// </summary>
+        [JsonProperty("permissions")]
+        public PdfMakePermissions Permissions { get; set; }
+        #endregion
 
+        #region Constructor
         /// <summary>
-        /// Sends a javascript script with the specified Iframe selector to display the pdf document
+        /// Default constructor
         /// </summary>
-        /// <param name="IFrameQuerySelector"></param>
-        /// <returns></returns>
-        public ContentResult EmbedInBrowserIframe(string IFrameQuerySelector)
+        public PdfMake()
         {
-            return new ContentResult()
-            {
-                Content = GetEmbedInBrowserIframe(IFrameQuerySelector),
-                ContentType = "application/javascript",
-                StatusCode = 200
-            };
-        }
-
-        /// <summary>
-        /// Sends a javascript script to open the pdf document in the browser
-        /// </summary>
-        /// <param name="SameWindow"></param>
-        /// <returns></returns>
-        public ContentResult OpenInBrowser(bool SameWindow)
-        {
-            return new ContentResult()
-            {
-                Content = GetOpenInBrowser(SameWindow),
-                ContentType = "application/javascript",
-                StatusCode = 200
-            };
-        }
-
-        /// <summary>
-        /// Sends a javascript script to prompt the printer with the pdf document
-        /// </summary>
-        /// <param name="SameWindow"></param>
-        /// <returns></returns>
-        public ContentResult PrintInBrowser(bool SameWindow)
-        {
-            return new ContentResult()
-            {
-                Content = GetPrintInBrowser(SameWindow),
-                ContentType = "application/javascript",
-                StatusCode = 200
-            };
+            Body = new List<object>();
+            Header = new List<object>();
+            Footer = new List<object>();
+            PageSize = PdfMakeNet.PageSize.LETTER;
         }
         #endregion
 
-        #region String Document Definitions
+        #region Body
         /// <summary>
-        /// Gets the document definition in json
+        /// Adds a qr code to the body section
         /// </summary>
-        /// <returns></returns>
-        public string GetDocumentDefinition()
+        /// <param name="qRCode"></param>
+        public void AddQRCode(PdfMakeQRCode qRCode)
         {
-            return Newtonsoft.Json.JsonConvert.SerializeObject(this);
+            Body.Add(qRCode);
         }
-
         /// <summary>
-        /// Gets the javascript script to dowload the pdf document
+        /// Adds a text to the body section
         /// </summary>
-        /// <param name="Filename"></param>
-        /// <returns></returns>
-        public string GetDownloadInBrowser(string Filename)
+        /// <param name="PdfMakeText"></param>
+        public void AddText(PdfMakeText PdfMakeText)
         {
-            if (string.IsNullOrWhiteSpace(Filename))
-                throw new ArgumentNullException("The Filename is null, empty or whitespace.");
-
-            if (!Filename.EndsWith(".pdf"))
-                throw new FormatException("The Filename does not end with .pdf extension.");
-
-            return "pdfMake.createPdf(" + GetDocumentDefinition() + ").download(" + Filename + ");";
+            Body.Add(PdfMakeText);
         }
-
         /// <summary>
-        /// Gets the javascript script to display the pdf document in iframe
+        /// Add a list of texts to the body section
         /// </summary>
-        /// <param name="IFrameQuerySelector"></param>
-        /// <returns></returns>
-        public string GetEmbedInBrowserIframe(string IFrameQuerySelector)
+        /// <param name="PdfMakeTexts"></param>
+        public void AddText(IEnumerable<PdfMakeText> PdfMakeTexts)
         {
-            if (string.IsNullOrWhiteSpace(IFrameQuerySelector))
-                throw new ArgumentNullException("The IframeQuerySelector is null, empty or whitespace.");
-
-            return @"pdfMake.createPdf(" + GetDocumentDefinition() + @").getDataUrl(function(dataUrl) {
-                        document.querySelector('" + IFrameQuerySelector + @"').src = dataUrl
-                    });";
+            Body.Add(new { text = PdfMakeTexts });
         }
-
         /// <summary>
-        /// Gets the javascript script to open in window
+        /// Adds columns to the body section
         /// </summary>
-        /// <param name="SameWindow"></param>
-        /// <returns></returns>
-        public string GetOpenInBrowser(bool SameWindow)
+        /// <param name="Columns"></param>
+        public void AddColumns(PdfMakeColumns<object> Columns)
         {
-            if (SameWindow)
-            {
-                return "pdfMake.createPdf(" + GetDocumentDefinition() + ").open({}, window);";
-            }
-            else
-            {
-                return "pdfMake.createPdf(" + GetDocumentDefinition() + ").open({}, window.open('', '_blank'));";
-            }
+            Body.Add(Columns);
         }
-
         /// <summary>
-        /// Gets the javascript script to prompt the printer with the pdf document
+        /// Adds a table to the body section
         /// </summary>
-        /// <param name="SameWindow"></param>
-        /// <returns></returns>
-        public string GetPrintInBrowser(bool SameWindow)
+        /// <param name="pdfMakeTable"></param>
+        public void AddTable(PdfMakeTable<object> pdfMakeTable)
         {
-            if (SameWindow)
-            {
-                return "pdfMake.createPdf(" + GetDocumentDefinition() + ").print({}, window);";
-            }
-            else
-            {
-                return "pdfMake.createPdf(" + GetDocumentDefinition() + ").print({}, window.open('', '_blank'));";
-            }
+            Body.Add(pdfMakeTable);
+        }
+        /// <summary>
+        ///  Adds a ordered list to the body section
+        /// </summary>
+        /// <param name="pdfMakeOrderedList"></param>
+        public void AddOrderedList(PdfMakeOrderedList<object> pdfMakeOrderedList)
+        {
+            Body.Add(pdfMakeOrderedList);
+        }
+        /// <summary>
+        /// Adds a unordered list to the body section
+        /// </summary>
+        /// <param name="pdfMakeUnorderedList"></param>
+        public void AddUnorderedList(PdfMakeUnorderedList<object> pdfMakeUnorderedList)
+        {
+            Body.Add(pdfMakeUnorderedList);
+        }
+        /// <summary>
+        /// Adds a stack to the body section
+        /// </summary>
+        /// <param name="pdfMakeStacks"></param>
+        public void AddStack(PdfMakeStack<object> pdfMakeStacks)
+        {
+            Body.Add(new { pdfMakeStacks });
+        }
+        /// <summary>
+        /// Adds an image to the body section
+        /// </summary>
+        /// <param name="pdfMakeImage"></param>
+        public void AddImage(PdfMakeImage pdfMakeImage)
+        {
+            Body.Add(pdfMakeImage);
+        }
+        /// <summary>
+        /// Adds a link to the body section
+        /// </summary>
+        /// <param name="pdfMakeLink"></param>
+        public void AddLink(PdfMakeLink pdfMakeLink)
+        {
+            Body.Add(pdfMakeLink);
+        }
+        /// <summary>
+        /// Adds a table of content to the body section
+        /// </summary>
+        /// <param name="pdfMakeTableOfContent"></param>
+        public void AddTableOfContent(PdfMakeTableOfContent pdfMakeTableOfContent)
+        {
+            Body.Add(pdfMakeTableOfContent);
         }
         #endregion
+
+        #region Header
+        /// <summary>
+        /// Adds a qr code to the header section
+        /// </summary>
+        /// <param name="qRCode"></param>
+        public void AddHeaderQRCode(PdfMakeQRCode qRCode)
+        {
+            Header.Add(qRCode);
+        }
+        /// <summary>
+        /// Adds a text to the header section
+        /// </summary>
+        /// <param name="PdfMakeText"></param>
+        public void AddHeaderText(PdfMakeText PdfMakeText)
+        {
+            Header.Add(PdfMakeText);
+        }
+        /// <summary>
+        /// Adds list of texts to the header section
+        /// </summary>
+        /// <param name="PdfMakeText"></param>
+        public void AddHeaderText(IEnumerable<PdfMakeText> PdfMakeTexts)
+        {
+            Header.Add(new { text = PdfMakeTexts });
+        }
+        /// <summary>
+        ///  Adds text to the header section
+        /// </summary>
+        /// <param name="Columns"></param>
+        public void AddHeaderColumns(PdfMakeColumns<object> Columns)
+        {
+            Header.Add(Columns);
+        }
+        /// <summary>
+        /// Adds a table to the header section
+        /// </summary>
+        /// <param name="pdfMakeTable"></param>
+        public void AddHeaderTable(PdfMakeTable<object> pdfMakeTable)
+        {
+            Header.Add(pdfMakeTable);
+        }
+        /// <summary>
+        /// Adds a ordered list to the header section
+        /// </summary>
+        /// <param name="pdfMakeOrderedList"></param>
+        public void AddHeaderOrderedList(PdfMakeOrderedList<object> pdfMakeOrderedList)
+        {
+            Header.Add(pdfMakeOrderedList);
+        }
+        /// <summary>
+        /// Adds a unordered list to the header section
+        /// </summary>
+        /// <param name="pdfMakeUnorderedList"></param>
+        public void AddHeaderUnorderedList(PdfMakeUnorderedList<object> pdfMakeUnorderedList)
+        {
+            Header.Add(pdfMakeUnorderedList);
+        }
+        /// <summary>
+        /// Adds a stack to the header section
+        /// </summary>
+        /// <param name="pdfMakeStacks"></param>
+        public void AddHeaderStack(PdfMakeStack<object> pdfMakeStacks)
+        {
+            Header.Add(new { pdfMakeStacks });
+        }
+        /// <summary>
+        /// Adds an image to the header section
+        /// </summary>
+        /// <param name="pdfMakeImage"></param>
+        public void AddHeaderImage(PdfMakeImage pdfMakeImage)
+        {
+            Header.Add(pdfMakeImage);
+        }
+        /// <summary>
+        /// Adds a link to the header section
+        /// </summary>
+        /// <param name="pdfMakeLink"></param>
+        public void AddHeaderLink(PdfMakeLink pdfMakeLink)
+        {
+            Header.Add(pdfMakeLink);
+        }
+        #endregion
+
+        #region Footer
+        /// <summary>
+        /// Adds a qr code to the footer section
+        /// </summary>
+        /// <param name="qRCode"></param>
+        public void AddFooterQRCode(PdfMakeQRCode qRCode)
+        {
+            Footer.Add(qRCode);
+        }
+        /// <summary>
+        /// Adds a text to the footer section
+        /// </summary>
+        /// <param name="PdfMakeText"></param>
+        public void AddFooterText(PdfMakeText PdfMakeText)
+        {
+            Footer.Add(PdfMakeText);
+        }
+        /// <summary>
+        /// Adds a list of texts to the footer section
+        /// </summary>
+        /// <param name="PdfMakeTexts"></param>
+        public void AddFooterText(IEnumerable<PdfMakeText> PdfMakeTexts)
+        {
+            Footer.Add(new { text = PdfMakeTexts });
+        }
+        /// <summary>
+        /// Adds a columns to the footer section
+        /// </summary>
+        /// <param name="Columns"></param>
+        public void AddFooterColumns(PdfMakeColumns<object> Columns)
+        {
+            Footer.Add(Columns);
+        }
+        /// <summary>
+        /// Adds a table to the footer section
+        /// </summary>
+        /// <param name="pdfMakeTable"></param>
+        public void AddFooterTable(PdfMakeTable<object> pdfMakeTable)
+        {
+            Footer.Add(pdfMakeTable);
+        }
+        /// <summary>
+        /// Adds a ordered list to the footer section
+        /// </summary>
+        /// <param name="pdfMakeOrderedList"></param>
+        public void AddFooterOrderedList(PdfMakeOrderedList<object> pdfMakeOrderedList)
+        {
+            Footer.Add(pdfMakeOrderedList);
+        }
+        /// <summary>
+        /// Adds a unordered list to the footer section
+        /// </summary>
+        /// <param name="pdfMakeUnorderedList"></param>
+        public void AddFooterUnorderedList(PdfMakeUnorderedList<object> pdfMakeUnorderedList)
+        {
+            Footer.Add(pdfMakeUnorderedList);
+        }
+        /// <summary>
+        /// Adds a stack to the footer section
+        /// </summary>
+        /// <param name="pdfMakeStacks"></param>
+        public void AddFooterStack(PdfMakeStack<object> pdfMakeStacks)
+        {
+            Footer.Add(new { pdfMakeStacks });
+        }
+        /// <summary>
+        /// Adds an image to the footer section
+        /// </summary>
+        /// <param name="pdfMakeImage"></param>
+        public void AddFooterImage(PdfMakeImage pdfMakeImage)
+        {
+            Footer.Add(pdfMakeImage);
+        }
+        /// <summary>
+        /// Adds a link to the footer section
+        /// </summary>
+        /// <param name="pdfMakeLink"></param>
+        public void AddFooterLink(PdfMakeLink pdfMakeLink)
+        {
+            Footer.Add(pdfMakeLink);
+        }
+        #endregion 
     }
 }
