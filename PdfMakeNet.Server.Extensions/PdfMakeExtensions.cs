41580c6a3c218017423b33bf2235a096bc427fee
jacob.rc021995 <jacob.rc021995@hotmail.com>
2020-05-25 21:43:23 -0400

    Added Blazor Demo. Added Server Extensions.

---
+++ PdfMakeNet.Server.Extensions/PdfMakeExtensions.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PdfMakeNet.Server.Extensions
+{
+    public static class PdfMakeExtensions
+    {
+        /// <summary>
+        /// Sends a json result with the pdf document definition
+        /// </summary>
+        /// <returns></returns>
+        public static ContentResult DocumentDefinitionInBrowser(this PdfMake pdfMake)
+        {
+            return new ContentResult()
+            {
+                Content = pdfMake.GetDocumentDefinition(),
+                ContentType = "application/json",
+                StatusCode = 200
+            };
+        }
+        /// <summary>
+        /// Sends a javascript script to download the pdf document in browser 
+        /// </summary>
+        /// <param name="Filename"></param>
+        /// <returns></returns>
+        public static ContentResult DownloadInBrowser(this PdfMake pdfMake, string Filename)
+        {
+            return new ContentResult()
+            {
+                Content = pdfMake.GetDownloadInBrowser(Filename),
+                ContentType = "application/javascript",
+                StatusCode = 200
+            };
+        }
+
+        /// <summary>
+        /// Sends a javascript script with the specified Iframe selector to display the pdf document
+        /// </summary>
+        /// <param name="IFrameQuerySelector"></param>
+        /// <returns></returns>
+        public static ContentResult EmbedInBrowserIframe(this PdfMake pdfMake, string IFrameQuerySelector)
+        {
+            return new ContentResult()
+            {
+                Content = pdfMake.GetEmbedInBrowserIframe(IFrameQuerySelector),
+                ContentType = "application/javascript",
+                StatusCode = 200
+            };
+        }
+
+        /// <summary>
+        /// Sends a javascript script to open the pdf document in the browser
+        /// </summary>
+        /// <param name="SameWindow"></param>
+        /// <returns></returns>
+        public static ContentResult OpenInBrowser(this PdfMake pdfMake, bool SameWindow)
+        {
+            return new ContentResult()
+            {
+                Content = pdfMake.GetOpenInBrowser(SameWindow),
+                ContentType = "application/javascript",
+                StatusCode = 200
+            };
+        }
+
+        /// <summary>
+        /// Sends a javascript script to prompt the printer with the pdf document
+        /// </summary>
+        /// <param name="SameWindow"></param>
+        /// <returns></returns>
+        public static ContentResult PrintInBrowser(this PdfMake pdfMake, bool SameWindow)
+        {
+            return new ContentResult()
+            {
+                Content = pdfMake.GetPrintInBrowser(SameWindow),
+                ContentType = "application/javascript",
+                StatusCode = 200
+            };
+        }
+    }
+}
--- PdfMakeNet/Extensions/PdfMakeExtensions.cs
+++ PdfMakeNet/Extensions/PdfMakeExtensions.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
@@ -8,116 +7,41 @@ namespace PdfMakeNet
 {
     public static class PdfMakeExtensions
     {
+
         /// <summary>
         /// Extension method to help you build tables more faster an easier without the hassle of loops and structure
         /// </summary>
         /// <param name="Columns"></param>
         /// <param name="Rows"></param>
         /// <returns></returns>
-        public static List<object> AddTableBodyStructure(this PdfMake pdfMake, List<string> Columns, List<object> Rows)
-        {
-            var body = new List<object>
-            {
-                Columns
-            };
-            foreach (var items in Rows)
-            {
-                var values = new List<object>();
-                var type = items.GetType();
-                if (type.GetGenericArguments()[0].IsSimpleType() && !type.IsAnonymousType())
-                {
-                    foreach (var item in (dynamic)items)
-                    {
-                        values.Add(item);
-                    }
-                }
-                else
-                {
-                    foreach (PropertyInfo prop in items.GetType().GetProperties())
-                    {
-                        values.Add(items.GetType().GetProperty(prop.Name).GetValue(items));
-                    }
-                }
-                body.Add(values);
-            }
-            return body;
-        }
-        
-        #region Client Side Content Result
-        /// <summary>
-        /// Sends a json result with the pdf document definition
-        /// </summary>
-        /// <returns></returns>
-        public static ContentResult DocumentDefinitionInBrowser(this PdfMake pdfMake)
-        {
-            return new ContentResult()
-            {
-                Content = pdfMake.GetDocumentDefinition(),
-                ContentType = "application/json",
-                StatusCode = 200
-            };
-        }
-        /// <summary>
-        /// Sends a javascript script to download the pdf document in browser 
-        /// </summary>
-        /// <param name="Filename"></param>
-        /// <returns></returns>
-        public static ContentResult DownloadInBrowser(this PdfMake pdfMake, string Filename)
-        {
-            return new ContentResult()
-            {
-                Content = pdfMake.GetDownloadInBrowser(Filename),
-                ContentType = "application/javascript",
-                StatusCode = 200
-            };
-        }
-
-        /// <summary>
-        /// Sends a javascript script with the specified Iframe selector to display the pdf document
-        /// </summary>
-        /// <param name="IFrameQuerySelector"></param>
-        /// <returns></returns>
-        public static ContentResult EmbedInBrowserIframe(this PdfMake pdfMake, string IFrameQuerySelector)
-        {
-            return new ContentResult()
-            {
-                Content = pdfMake.GetEmbedInBrowserIframe(IFrameQuerySelector),
-                ContentType = "application/javascript",
-                StatusCode = 200
-            };
-        }
-
-        /// <summary>
-        /// Sends a javascript script to open the pdf document in the browser
-        /// </summary>
-        /// <param name="SameWindow"></param>
-        /// <returns></returns>
-        public static ContentResult OpenInBrowser(this PdfMake pdfMake, bool SameWindow)
-        {
-            return new ContentResult()
-            {
-                Content = pdfMake.GetOpenInBrowser(SameWindow),
-                ContentType = "application/javascript",
-                StatusCode = 200
-            };
-        }
-
-        /// <summary>
-        /// Sends a javascript script to prompt the printer with the pdf document
-        /// </summary>
-        /// <param name="SameWindow"></param>
-        /// <returns></returns>
-        public static ContentResult PrintInBrowser(this PdfMake pdfMake, bool SameWindow)
-        {
-            return new ContentResult()
-            {
-                Content = pdfMake.GetPrintInBrowser(SameWindow),
-                ContentType = "application/javascript",
-                StatusCode = 200
-            };
-        }
-        #endregion
-
+        //public static List<object> AddTableBodyStructure(this PdfMake pdfMake, List<string> Columns, List<object> Rows)
+        //{
+        //    var body = new List<object>
+        //    {
+        //        Columns
+        //    };
+        //    foreach (var items in Rows)
+        //    {
+        //        var values = new List<object>();
+        //        var type = items.GetType();
+        //        if (type.GetGenericArguments()[0].IsSimpleType() && !type.IsAnonymousType())
+        //        {
+        //            foreach (var item in items)
+        //            {
+        //                values.Add((object)item);
+        //            }
+        //        }
+        //        else
+        //        {
+        //            foreach (PropertyInfo prop in items.GetType().GetProperties())
+        //            {
+        //                values.Add(items.GetType().GetProperty(prop.Name).GetValue(items));
+        //            }
+        //        }
+        //        body.Add(values);
+        //    }
+        //    return body;
+        //}
         #region String Document Definitions
         /// <summary>
         /// Gets the document definition in json
