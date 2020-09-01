4ff9cf4e064d1ad366d9cf4b9f64818622b74687
chandan singh <chandan.singh@adp.com>
2020-09-01 22:16:20 +0530

    feat #1: lists demo code added

---
+++ PdfMakeNet/Constants/OrderedListType.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PdfMakeNet
+{
+    public sealed class OrderedListType
+    {
+        private static readonly Dictionary<string, OrderedListType> Instance = new Dictionary<string, OrderedListType>();
+        private readonly string name;
+
+        private OrderedListType(string name)
+        {
+            this.name = name;
+            Instance[name] = this;
+        }
+
+        public static implicit operator OrderedListType(string str)
+        {
+            return $"{FromString(str)}";
+        }
+
+        public static OrderedListType FromString(string str)
+        {
+            if (Instance.TryGetValue(str, out OrderedListType result))
+            {
+                return result;
+            }
+            else
+            {
+                throw new ArgumentException($"{nameof(OrderedListType)} must be one of the followings https://pdfmake.github.io/docs/");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.name}";
+        }
+
+        public static readonly OrderedListType LowerAlpha = new OrderedListType("lower-alpha");
+        public static readonly OrderedListType UpperAlpha = new OrderedListType("upper-alpha");
+        public static readonly OrderedListType LowerRoman = new OrderedListType("lower-roman");
+        public static readonly OrderedListType UpperRoman = new OrderedListType("upper-roman" );
+        public static readonly OrderedListType None = new OrderedListType("none");
+    }
+}
